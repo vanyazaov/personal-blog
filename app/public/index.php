d9d8594ec8edd84d6a380fb0784671374c664621
Ivan Zayashnikov <vanyazaov@gmail.com>
2026-05-07 11:13:11 +0300

    Улучшена реализация FrontController с добавлением роутера на массиве и разбором через функцию dispatch. Это подготовительный этап для последующего выноса роутеров в отдельный файл как и функцию их диспечеризации.

--- app/public/index.php
+++ app/public/index.php
@@ -10,21 +10,46 @@ $method = $_SERVER['REQUEST_METHOD'];
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 /**
- * Использование паттерна Front Controller с динамическим параметром на основе регулярных выражений
+ * Использование паттерна Front Controller с роутером на массиве
  */
-if ($method === 'GET' && $path === '/') {
-    echo homePage();
-}
-elseif ($method === 'GET' && preg_match('#^/category/(\d+)$#', $path, $m)) {
-    echo showArticleList((int) $m[1]);
-} else {
+$routes = [
+    ['GET', '/', 'homePage'],
+    ['GET', '/category/{id}' , 'showArticleList'],
+];
+
+dispatch($routes, $method, $path);
+
+function dispatch(array $routes, string $method, string $path): void
+{
+    foreach ($routes as [$routeMethod, $routePath, $routeAction]) {
+        if ($routeMethod !== $method) { continue; }
+
+        // из $routePath делаем выражение для regex с именованной группой
+        $pattern = '#^' . preg_replace('#\{(\w+)\}#', '(?P<$1>[^/]+)', $routePath) . '$#';
+        
+        // Ищем совпадения по созданному паттерну
+        if (preg_match($pattern, $path, $matches)) {
+            // Передаем параметры в функцию
+            if (count($matches) >= 3) {
+               echo call_user_func($routeAction, $matches[1]); 
+            } else {
+               echo call_user_func($routeAction); 
+            }
+            exit();
+        }
+    }
+
+    // Ничего не нашлось
+    http_response_code(404);
     echo notFound();
 }
+
 
 function homePage(): string {
     return "<h1>Рад вас видеть на моём блоге!</h1>";
 }
-function showArticleList(int $category_id): string {
+function showArticleList(string $category_id): string {
+    $category_id = (int)$category_id;
     return "<h1>Список статей в категории №$category_id</h1>";
 }
 function notFound(): string {
